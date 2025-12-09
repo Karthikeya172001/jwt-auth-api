25c433dabd865812debc3dcb27cc185f902a19df
Karthikeya <73694993+Karthikeya172001@users.noreply.github.com>
2025-12-09 08:56:51 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,7 +1,14 @@
+
 from pymongo import MongoClient
+from dotenv import load_dotenv
 import os
 
-client = MongoClient(os.getenv("MONGO_URI"))
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI")
+client = MongoClient(MONGO_URI)
+
 db = client["auth_db"]
 users_collection = db["users"]
+
 
